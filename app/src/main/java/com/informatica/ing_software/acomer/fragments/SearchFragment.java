87c954b05992b72072a7ad37d171d30ecca72aed
Diego Gonzalez <gadiego92@gmail.com>
2017-05-22 17:35:44 +0200

    Search Bar. Started

--- app/src/main/java/com/informatica/ing_software/acomer/fragments/SearchFragment.java
+++ app/src/main/java/com/informatica/ing_software/acomer/fragments/SearchFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -149,6 +150,15 @@ public class SearchFragment extends Fragment {
 
                 // To avoid onItemClick event after onItemLongClick
                 return true;
+            }
+        });
+
+        // Create a listener for the search bar
+        SearchView searchBar = (SearchView) view.findViewById(R.id.fSearchSearchView);
+        searchBar.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Bien", Toast.LENGTH_LONG).show();
             }
         });
 
